df68fa95cd560f42e5ea0bc60f871b5033859af1
Krzysztof Gryc <hello@krzysztofg.pl>
2020-08-08 10:12:20 +0200

    handleCommand WiP

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-parseArgs = require('minimist');
+const parseArgs = require('minimist');
+const colors = require('colors');
 
 const command = parseArgs(process.argv.slice(2,3));
 delete command._;
 
 const handleCommand = ({ add, remove, list }) => {
+    if(add){
+        if(typeof (add) !== 'string'){
+            return console.log('Wpisz treść zadania w postaci tekstu'.red);
+        } else {
+            console.log(`Twoje zadanie: ${add.green}`)
+        }
+    } else if(remove){
+
+    } else if(list){
+
+    } else {
+
+    }
     console.log(add, remove, list)
 };
 
