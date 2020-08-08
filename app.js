811c1cf0abf0f546102599a0770530574bba85a8
Krzysztof Gryc <hello@krzysztofg.pl>
2020-08-08 10:26:29 +0200

    end basic logic handleCommand

--- app.js
+++ app.js
@@ -6,19 +6,32 @@ delete command._;
 
 const handleCommand = ({ add, remove, list }) => {
     if(add){
-        if(typeof (add) !== 'string'){
+        if(typeof(add) !== 'string'){
             return console.log('Wpisz treść zadania w postaci tekstu'.red);
+        } else if (add.length < 7){
+            return console.log('Tekst musi mieć minimum 6 znaków'.red);
         } else {
             console.log(`Twoje zadanie: ${add.green}`)
+            handleData(add)
         }
     } else if(remove){
-
-    } else if(list){
-
+        if(typeof(remove) !== 'string' || remove.length < 7){
+            return console.log('Wpisz nazwę zadania do usunięcia. Zadania mają długość nazwy z minimum 6 znaków.'.red)
+        } else {
+            console.log(`Usunięte zadanie: ${remove.green}`)
+            handleData(remove)
+        }
+    } else if(list || list === ""){
+        console.log('Pokazuję listę'.green)
+        handleData(list)
     } else {
-
+        console.log('Nie rozumiem polecenia'.red)
     }
-    console.log(add, remove, list)
+
 };
+
+const handleData = (data) =>{
+    console.log(data)
+}
 
 handleCommand(command);
