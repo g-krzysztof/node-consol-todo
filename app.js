6c16e81a1d0e5ee362a8f00b7c889ab3f8415a9b
Krzysztof Gryc <hello@krzysztofg.pl>
2020-08-08 09:47:54 +0200

    add npm minimist, start handleCommand feature

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log(process.argv);
+parseArgs = require('minimist');
+
+const command = parseArgs(process.argv.slice(2,3));
+delete command._;
+
+const handleCommand = ({ add, remove, list }) => {
+    console.log(add, remove, list)
+};
+
+handleCommand(command);
