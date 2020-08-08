280bbd6e376fa15db7e5034c9e1a60b88406b7d1
Krzysztof Gryc <hello@krzysztofg.pl>
2020-08-08 11:24:20 +0200

    add write to db feature

--- app.js
+++ app.js
@@ -12,18 +12,15 @@ const handleCommand = ({ add, remove, list }) => {
         } else if (add.length < 7){
             return console.log('Tekst musi mieć minimum 6 znaków'.red);
         } else {
-            console.log(`Twoje zadanie: ${add.green}`)
             handleData(1, add)
         }
     } else if(remove){
         if(typeof(remove) !== 'string' || remove.length < 7){
             return console.log('Wpisz nazwę zadania do usunięcia. Zadania mają długość nazwy z minimum 6 znaków.'.red)
         } else {
-            console.log(`Usunięte zadanie: ${remove.green}`)
             handleData(2, remove)
         }
     } else if(list || list === ""){
-        console.log('Pokazuję listę'.green)
         handleData(3, null)
     } else {
         console.log('Nie rozumiem polecenia'.red)
@@ -37,7 +34,36 @@ const handleData = (type, task) =>{
 
     const data = fs.readFileSync('./taskdb.json');
     const tasks = JSON.parse(data);
-    console.log(tasks)
+    // console.log(tasks)
+
+    if(type === 1 || type === 2) {
+        const isExisted = tasks.find(item => item.task === task) ? true : false;
+
+        if(type === 1 && isExisted){
+            return console.log('Takie zadanie już istnieje'.red)
+        } else if (type === 2 && !isExisted){
+            return console.log('Takie zadanie nie istnieje'.red)
+        }
+    }
+
+    switch(type){
+        case 1:
+            console.log('Dodaję zadanie'.green)
+            const id = tasks.length + 1;
+            tasks.push({
+                id, task
+            });
+            const dataJSON = JSON.stringify(tasks);
+            fs.writeFileSync('./taskdb.json', dataJSON)
+            break;
+        case 2:
+            console.log('Usuwam zadanie'.green)
+            break;
+        case 3:
+            console.log('Pokazuję listę'.green)
+            break;
+    }
+
 }
 
 handleCommand(command);
